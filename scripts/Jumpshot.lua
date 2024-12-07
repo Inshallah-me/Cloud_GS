b885d14033c4da7acb032f86ae53cccda057cb9d
Inshallah <106265222+Inshallah-me@users.noreply.github.com>
2024-12-08 05:39:03 +0800

    Update Jumpshot.txt

--- scripts/Jumpshot.lua
+++ scripts/Jumpshot.lua
@@ -1,10 +1,1 @@
-local air_strafe = ui.reference("Misc", "Movement", "Air strafe")
-local enable_it = ui.new_checkbox("Misc", "Movement", "Jumpscout")
-
-client.set_event_callback("setup_command", function(c)
-    if (ui.get(enable_it)) then
-        local vel_x, vel_y = entity.get_prop(entity.get_local_player(), "m_vecVelocity")
-        local vel = math.sqrt(vel_x^2 + vel_y^2)
-        ui.set(air_strafe, not (c.in_jump and (vel < 10)) or ui.is_menu_open())
-    end
-end)
+"local air_strafe = ui.reference(\"Misc\", \"Movement\", \"Air strafe\")\nlocal enable_it = ui.new_checkbox(\"Misc\", \"Movement\", \"Jumpscout\")\n\nclient.set_event_callback(\"setup_command\", function(c)\n    if (ui.get(enable_it)) then\n        local vel_x, vel_y = entity.get_prop(entity.get_local_player(), \"m_vecVelocity\")\n        local vel = math.sqrt(vel_x^2 + vel_y^2)\n        ui.set(air_strafe, not (c.in_jump and (vel < 10)) or ui.is_menu_open())\n    end\nend)\n"
